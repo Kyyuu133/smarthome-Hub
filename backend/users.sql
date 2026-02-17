8e1bc3341e72f8010ed625557b54c1880f3c4a2e
Kyyuu133 <kyyuu@outlook.de>
2026-02-17 10:15:19 +0100

    class in main.py hinzugefügt delete einzelne datenbanken

--- backend/users.sql
+++
@@ -1,9 +0,0 @@
-#  SQLite Datenbank für Users
-
-CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_name TEXT NOT NULL UNIQUE,
-    user_passwort TEXT NOT NULL,
-    rolle TEXT NOT NULL CHECK(rolle IN ('admin', 'user'))
-);
-
