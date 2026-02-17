b328de3c91fedca4b2a8e0d7f12369026d32dd5a
NvK-Bit <clubvenaca@gmail.com>
2026-02-17 09:08:09 +0100

    Datenbanken zusammenfeführt

---
+++ backend/useres_rooms_devices.sql
@@ -0,0 +1,27 @@
+PRAGMA foreign_keys = ON;
+
+-- 1. Users Tabelle
+CREATE TABLE IF NOT EXISTS users (
+    user_id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_name     TEXT    NOT NULL UNIQUE,
+    user_passwort TEXT    NOT NULL,
+    rolle         TEXT    NOT NULL CHECK(rolle IN ('admin', 'user'))
+);
+
+-- 2. Rooms Tabelle
+CREATE TABLE IF NOT EXISTS rooms (
+    room_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    room_name VARCHAR(50) NOT NULL UNIQUE,
+    user_id   INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- 3. Devices Tabelle
+CREATE TABLE IF NOT EXISTS devices (
+    device_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    room_id     INTEGER,
+    device_name TEXT    NOT NULL UNIQUE,
+    device_type TEXT    NOT NULL,
+    status      BOOLEAN NOT NULL DEFAULT 0,
+    FOREIGN KEY (room_id) REFERENCES rooms(room_id)
+);
