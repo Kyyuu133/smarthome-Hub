e1b270c4404e9cdb547a4cf34c97b6626a99cd0a
lyz <t.adickes@protonmail.com>
2026-02-18 08:58:27 +0100

    aktualisiert mit Kommis

--- backend/user_api.py
+++ backend/user_api.py
@@ -5,20 +5,23 @@ from typing import Optional
 import sqlite3
 
 app = FastAPI()
-
+#connect to db function
 def get_db():
     conn = sqlite3.connect("hub.db")
     conn.row_factory = sqlite3.Row
     curs = conn.cursor()
     return conn, curs
-
+#class User
 class User(BaseModel):
     user_name: str
     user_passwort: str
     rolle: Optional[str] = "user"
 
+#add user post request
 @app.post("/add_user")
 async def add_user(user: User):
+    
+    #username and password check
     username_result = username_check(user.user_name)
     if username_result != user.user_name:
         return {"error": username_result}
@@ -28,14 +31,14 @@ async def add_user(user: User):
         return {"error": password_result}
 
     conn, curs = get_db()
-
+    #Check for Existing first user(admin)
     existing = curs.execute("SELECT user_id FROM users WHERE user_id = 1").fetchone()
 
     if not existing:
         rolle = "admin"
     else:
         rolle = user.rolle
-
+    #Add new user
     curs.execute(
         "INSERT INTO users (user_name, user_passwort, rolle) VALUES (?, ?, ?)",
         (user.user_name, user.user_passwort, rolle)
@@ -44,3 +47,4 @@ async def add_user(user: User):
     conn.close()
 
     return {"message": f"User '{user.user_name}' wurde als {rolle} angelegt"}
+
